3ff07c0ced3346909d3c9336a5509c3e8ff9b88b
hoangdat2407 <82460838+hoangdat2407@users.noreply.github.com>
2025-10-28 14:00:11 +0700

    u7 p1 add blur o matic show final work

--- Unit7_Pathway1_background_work_with_work_manager/basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ Unit7_Pathway1_background_work_with_work_manager/basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,21 +19,66 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
+import com.example.bluromatic.workers.BlurWorker
+import com.example.bluromatic.workers.CleanupWorker
+import com.example.bluromatic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.work.ExistingWorkPolicy
+import com.example.bluromatic.IMAGE_MANIPULATION_WORK_NAME
+import com.example.bluromatic.TAG_OUTPUT
+import androidx.lifecycle.asFlow
+import kotlinx.coroutines.flow.mapNotNull
+
+
+
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
-    override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+    private var imageUri: Uri = context.getImageUri()
+    private val workManager = WorkManager.getInstance(context)
+
+    override val outputWorkInfo: Flow<WorkInfo> =
+        workManager.getWorkInfosByTagLiveData(TAG_OUTPUT).asFlow().mapNotNull {
+            if (it.isNotEmpty()) it.first() else null
+        }
 
     /**
      * Create the WorkRequests to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    override fun applyBlur(blurLevel: Int) {}
+    override fun applyBlur(blurLevel: Int) {
+        // Add WorkRequest to Cleanup temporary images
+        var continuation = workManager
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
+        // Add WorkRequest to blur the image
+        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+        // Input the Uri for the blur operation along with the blur level
+        blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+
+        continuation = continuation.then(blurBuilder.build())
+
+        // Add WorkRequest to save the image to the filesystem
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
+            .build()
+        continuation = continuation.then(save)
+
+        // Actually start the work
+        continuation.enqueue()
+    }
 
     /**
      * Cancel any ongoing WorkRequests
--- Unit7_Pathway1_background_work_with_work_manager/basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
+++ Unit7_Pathway1_background_work_with_work_manager/basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/ui/BlurViewModel.kt
@@ -16,6 +16,11 @@
 
 package com.example.bluromatic.ui
 
+
+
+import kotlinx.coroutines.flow.stateIn
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -26,6 +31,11 @@ import com.example.bluromatic.data.BlurAmountData
 import com.example.bluromatic.data.BluromaticRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import androidx.work.WorkInfo
+import kotlinx.coroutines.flow.map
+import com.example.bluromatic.KEY_IMAGE_URI
+
+
 
 /**
  * [BlurViewModel] starts and stops the WorkManger and applies blur to the image. Also updates the
@@ -35,7 +45,24 @@ class BlurViewModel(private val bluromaticRepository: BluromaticRepository) : Vi
 
     internal val blurAmount = BlurAmountData.blurAmount
 
-    val blurUiState: StateFlow<BlurUiState> = MutableStateFlow(BlurUiState.Default)
+//    val blurUiState: StateFlow<BlurUiState> = MutableStateFlow(BlurUiState.Default)
+    val blurUiState: StateFlow<BlurUiState> = bluromaticRepository.outputWorkInfo
+        .map { info ->
+            val outputImageUri = info.outputData.getString(KEY_IMAGE_URI)
+            when {
+                info.state.isFinished && !outputImageUri.isNullOrEmpty() -> {
+                BlurUiState.Complete(outputUri = outputImageUri)
+            }
+            info.state == WorkInfo.State.CANCELLED -> {
+                    BlurUiState.Default
+                }
+                else -> BlurUiState.Loading
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = BlurUiState.Default
+        )
 
     /**
      * Call the method from repository to create the WorkRequest to apply the blur
